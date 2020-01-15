521d293e09dc3829707692518403f19edb73830d
unknown <thiiagoac@hotmail.com>
2020-01-15 18:27:25 -0300

    criacao documento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { IndexComponent } from './index/index.component';
+import { DocumentoComponent } from './documento/documento.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { IndexComponent } from './index/index.component';
     HeaderComponent,
     MenuComponent,
     IndexComponent,
+    DocumentoComponent,
   ],
   imports: [
     FontAwesomeModule,
---
+++ src/app/documento/documento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-documento',
+  templateUrl: './documento.component.html',
+  styleUrls: ['./documento.component.css']
+})
+export class DocumentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
